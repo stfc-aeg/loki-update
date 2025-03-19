8ceacbebd77806c3ca98e0fc435382920a5c6625
CallumLee22 <callum.lee@stfc.ac.uk>
2025-03-19 13:26:11 +0000

    Fix identical image check

--- loki-update-ui/src/ImageInfo.jsx
+++ loki-update-ui/src/ImageInfo.jsx
@@ -91,7 +91,7 @@ export default function ImageInfo() {
                     </div>
                   </TitleCard>
                 </Col>
-                {!checkIdenticalPrimaryImages ? (
+                {checkIdenticalPrimaryImages ? (
                   <></>
                 ) : (
                   <Col>
