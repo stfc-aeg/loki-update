bec1a3a72947a5b20012a53171797c2b57e720ca
CallumLee22 <callum.lee@stfc.ac.uk>
2025-08-19 15:07:57 +0100

    Check image metadata at the top level first

--- src/loki_update/controller.py
+++ src/loki_update/controller.py
@@ -8,6 +8,7 @@ import tempfile
 import shutil
 import hashlib
 import requests
+import json
 
 from pyfdt.pyfdt import *
 
@@ -303,6 +304,8 @@ class LokiUpdateController():
         timestamp = ""
         error_occurred = False
         error_message = ""
+        u_boot_path = ""
+        dtb_path = ""
         
         if device == "emmc":
             u_boot_path = self.emmc_u_boot_path
@@ -315,17 +318,26 @@ class LokiUpdateController():
             dtb_path = self.backup_dtb_path
         
         try:
-            subprocess.run(["dumpimage", "-T", "flat_dt", "-p", "1", u_boot_path, "-o", dtb_path], capture_output=True, text=True, check=True)
-            
-            with open(dtb_path, "rb") as fdt_file:
-                fdt = FdtBlobParse(fdt_file)
-            
-            json_fdt =  json.loads(fdt.to_fdt().to_json())
+            # Check if metadata is at the top level
+            metadata_output = subprocess.run(["fdtget", "-p", u_boot_path, "/loki-metadata"], capture_output=True)
 
-            name = json_fdt["loki-metadata"]["application-name"][1]
-            app_version = json_fdt["loki-metadata"]["application-version"][1]
-            loki_version = json_fdt["loki-metadata"]["loki-version"][1]
-            platform = json_fdt["loki-metadata"]["platform"][1]
+            if metadata_output.returncode == 0:
+                name = subprocess.run(["fdtget", "-t", "s", u_boot_path, "/loki-metadata", "application-name"], capture_output=True, text=True, check=True).stdout.strip()
+                app_version = subprocess.run(["fdtget", "-t", "s", u_boot_path, "/loki-metadata", "application-version"], capture_output=True, text=True, check=True).stdout.strip()
+                loki_version = subprocess.run(["fdtget", "-t", "s", u_boot_path, "/loki-metadata", "loki-version"], capture_output=True, text=True, check=True).stdout.strip()
+                platform = subprocess.run(["fdtget", "-t", "s", u_boot_path, "/loki-metadata", "platform"], capture_output=True, text=True, check=True).stdout.strip()
+            else:
+                subprocess.run(["dumpimage", "-T", "flat_dt", "-p", "1", u_boot_path, "-o", dtb_path], capture_output=True, text=True, check=True)
+            
+                with open(dtb_path, "rb") as fdt_file:
+                    fdt = FdtBlobParse(fdt_file)
+                
+                json_fdt = json.loads(fdt.to_fdt().to_json())
+  
+                name = json_fdt["loki-metadata"]["application-name"][1]
+                app_version = json_fdt["loki-metadata"]["application-version"][1]
+                loki_version = json_fdt["loki-metadata"]["loki-version"][1]
+                platform = json_fdt["loki-metadata"]["platform"][1]
 
             timestamp_output = subprocess.run(["fdtget", "-t", "i", u_boot_path, "/", "timestamp"], capture_output=True, text=True)
             timestamp = timestamp_output.stdout.strip()
@@ -333,6 +345,7 @@ class LokiUpdateController():
         except subprocess.CalledProcessError as error:
             self.flash_error_occurred = True
             self.flash_error_message = str(error.stderr)
+            logging.error("Not flash")
             logging.error(f"{str(error.cmd)}: {self.flash_error_message}")
         
         except FileNotFoundError as error:
@@ -352,28 +365,37 @@ class LokiUpdateController():
     def get_flash_image_metadata_from_dtb(self):
         self.flash_loading = True
         with tempfile.TemporaryDirectory() as temp_dir:
-            kernel_mtddev = self.mtd_label_to_device("kernel")
-            
-            u_boot_path = temp_dir + "/image.ub"
-            dtb_path = temp_dir + "/system.dtb"
-            
             try:
-                os.system(f"cat {kernel_mtddev} > {u_boot_path}")
+                kernel_mtddev = self.mtd_label_to_device("kernel")
 
-                subprocess.run(["dumpimage", "-T", "flat_dt", "-p", "1", u_boot_path, "-o", dtb_path], capture_output=True, text=True, check=True)
-            
-                with open(dtb_path, "rb") as fdt_file:
-                    fdt = FdtBlobParse(fdt_file)
-            
-                json_fdt = json.loads(fdt.to_fdt().to_json())
-
-                self.flash_app_name = json_fdt["loki-metadata"]["application-name"][1]
-                self.flash_app_version = json_fdt["loki-metadata"]["application-version"][1]
-                self.flash_loki_version = json_fdt["loki-metadata"]["loki-version"][1]
-                self.flash_platform = json_fdt["loki-metadata"]["platform"][1]
+                # Check for metadata at the top level first
+                metadata_output = subprocess.run(["fdtget", "-t", "i", kernel_mtddev, "/", "loki-metadata"], capture_output=True)
+                if metadata_output.returncode == 0:  
+                    self.flash_app_name = subprocess.run(["fdtget", "-t", "s", kernel_mtddev, "/loki-metadata", "application-name"], capture_output=True, text=True, check=True).stdout.strip()
+                    self.flash_app_version = subprocess.run(["fdtget", "-t", "s", kernel_mtddev, "/loki-metadata", "application-version"], capture_output=True, text=True, check=True).stdout.strip()
+                    self.flash_loki_version = subprocess.run(["fdtget", "-t", "s", kernel_mtddev, "/loki-metadata", "loki-version"], capture_output=True, text=True, check=True).stdout.strip()
+                    self.flash_platform = subprocess.run(["fdtget", "-t", "s", kernel_mtddev, "/loki-metadata", "platform"], capture_output=True, text=True, check=True).stdout.strip()
+                    timestamp_output = subprocess.run(["fdtget", "-t", "i", kernel_mtddev, "/", "timestamp"], capture_output=True, text=True, check=True)
+                    self.flash_time_created = timestamp_output.stdout.strip()
+                else:                    
+                    u_boot_path = temp_dir + "/image.ub"
+                    dtb_path = temp_dir + "/system.dtb"
                 
-                timestamp_output = subprocess.run(["fdtget", "-t", "i", u_boot_path, "/", "timestamp"], capture_output=True, text=True, check=True)
-                self.flash_time_created = timestamp_output.stdout.strip()
+                    os.system(f"cat {kernel_mtddev} > {u_boot_path}")
+
+                    subprocess.run(["dumpimage", "-T", "flat_dt", "-p", "1", u_boot_path, "-o", dtb_path], capture_output=True, text=True, check=True)
+                
+                    with open(dtb_path, "rb") as fdt_file:
+                        fdt = FdtBlobParse(fdt_file)
+                    
+                    json_fdt = json.loads(fdt.to_fdt().to_json())
+    
+                    self.flash_app_name = json_fdt["loki-metadata"]["application-name"][1]
+                    self.flash_app_version = json_fdt["loki-metadata"]["application-version"][1]
+                    self.flash_loki_version = json_fdt["loki-metadata"]["loki-version"][1]
+                    self.flash_platform = json_fdt["loki-metadata"]["platform"][1]
+                    timestamp_output = subprocess.run(["fdtget", "-t", "i", u_boot_path, "/", "timestamp"], capture_output=True, text=True, check=True)
+                    self.flash_time_created = timestamp_output.stdout.strip()
             
             except subprocess.CalledProcessError as error:
                 self.flash_error_occurred = True
